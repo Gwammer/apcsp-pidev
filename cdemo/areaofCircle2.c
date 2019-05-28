479fc690795d2001635e9fd04e912533edd68668
Gwammer <33038959+Gwammer@users.noreply.github.com>
2019-05-27 18:39:50 -0700

    Update areaofCircle2.c

--- cdemo/areaofCircle2.c
+++ cdemo/areaofCircle2.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-float areaOfCircle(float radius) 
+void area_(double radius1, double radius2, double *area1, double *area2);
+
+int main(void)
+
 {
-  float area = (3.14159265358979323846 * radius * radius);
-  return area;
+	double radius1, radius2, area1, area2;
+
+	printf("Please enter the radius of the circle:\n");
+	scanf("%lf", &radius1);
+	scanf("%lf", &radius2);
+	area_(radius1, radius2, &area1, &area2);
+	if (area1 == 0) {
+		printf("Not a valid number, the value will return as 1\n");
+		area1 = 3.1400; }
+	if (area2 == 0) {
+		printf("Not a valid number, the value will return as 5\n");
+		area2 = 78.5000; }
+	printf("Area of circle: %0.4f and %0.4f respectively\n", area1, area2);
+	return 0;
 }
 
-int main(int argc, char* argv[])
+void area_(double radius1, double radius2, double *area1, double *area2)
+
 {
-  int arg = 0;
-  while (arg < argc)
-  {
-    printf("Bound %d is %s\n", arg, argv[arg]);
-    arg++;
-  }
-  float lower, upper;
-  printf("Enter a lower bound:\n");
-  scanf("%f", &lower);
-  printf("Enter an upper bound:\n");
-  scanf("%f", &upper);
-  for (float radius = lower; radius <= upper; radius++)
-    {
-	float result = areaOfCircle(radius);
-	printf("Area at r = %f: %f\n", radius, result);
-    }
+	double PIE = 3.14;
+	*area1 = PIE * radius1 * radius1;
+	*area2 = PIE * radius2 * radius2;
 }
