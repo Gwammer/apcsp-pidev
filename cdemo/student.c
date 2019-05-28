5b3ac85c7e9209916a94ec3f6b6599412ce44dce
Gwammer <33038959+Gwammer@users.noreply.github.com>
2019-05-27 20:04:42 -0700

    Update student.c

--- cdemo/student.c
+++ cdemo/student.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-#include "student.h"
 
-struct Student 
-{
-  char firstName;
-  char lastName;
-  int age;
-  int studentID;
-} Student;
+struct Student {
+	char first[50];
+	char last[50];
+	int age;
+	int stid;
+};
 
-char printStudent (struct Student* student)
+void printStudent(struct Student* student)
 {
-  printf("Name: %c %c\n", student->firstName, student->lastName);
-  printf("  Age: %d\n", student->age);
-  printf("  Student ID: %d\n", student->studentID);
-  return 0;
+	printf("The name is %s %s.\n", student -> first, student -> last);
+	printf("The age is %d\n", student -> age);
+	printf("The student ID is %d\n", student -> stid);
+
 }
 
 int main()
 {
-  struct Student JohnSmith;
-  struct Student JaneDoe;
-  char input, name;
 
-  strcpy(&JohnSmith.firstName, "John");
-  strcpy(&JohnSmith.lastName, "Smith");
-  JohnSmith.age = 16;
-  JohnSmith.studentID = 123456;
+	char input[50];
+	int answer;
+	printf("How many students?\n");
+	fgets(input, 256, stdin);
+	sscanf(input, "%d", &answer);
+	struct Student studentA[answer];
+	for (int i = 0; i<answer; i++)
+	{
+	printf("What is your first name?\n");
+	fgets(input, 256,stdin);
+	sscanf(input, "%s", &studentA[i].first);
 
-  strcpy(&JaneDoe.firstName, "Jane");
-  strcpy(&JaneDoe.lastName, "Doe");
-  JaneDoe.age = 15;
-  JaneDoe.studentID = 011235;
-  
-  printf("Which student(s)?\n");
-  fgets(&input, 256, stdin);
-  sscanf(&input, "%c", &name);
-  printStudent(struct Student* name);
+	printf("What is your last name?\n");
+	fgets(input, 256, stdin);
+	sscanf(input, "%s", &studentA[i].last);
+
+	printf("What is your age?\n");
+	fgets(input, 256, stdin);
+	sscanf(input, "%d", &studentA[i].age);
+
+	printf("What is your ID\n");
+	fgets(input, 256, stdin);
+	sscanf(input, "%d", &studentA[i].stid);
+	}
+
+	for (int i = 0; i<answer; i++)
+	{
+	printStudent(&studentA[i]);
+	}
 }
