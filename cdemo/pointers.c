f6c51e0478f99d4ff67caed53e29cc553bf67579
Gwammer <33038959+Gwammer@users.noreply.github.com>
2019-05-27 19:08:14 -0700

    Update pointers.c

--- cdemo/pointers.c
+++ cdemo/pointers.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
+
 int main()
 {
-	int a;
-	int* ptrtoa;
+  
+  int a;
+  int* ptrtoa;
 
-	ptrtoa = &a;
+  ptrtoa = &a;
 
-	a=5;
-	printf("The value of a is %d\n",a);
+  a = 5;
+  printf("The value of a is %d\n", a);
 
-	*ptrtoa = 6;
-	printf("The value of a is %d\n",a);
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
 
-	printf("The value of ptrtoa is %d\n", ptrtoa);
-	printf("It stores the value %d\n", *ptrtoa);
-	printf("The address of a is %d\n", &a);
+  printf("The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
 
-	int d;
-	int e;
-	int* add;
-	int* ade;
-	add = &d;
-	ade = &e;
-	d=18;
-	e=4;
-	printf("The value of d and e is %d and %d\n",d,e);
-	printf("The address of d and e is %d and %d\n",&d,&e);
-
-	int temp;
-	temp=d;
-	*add=e;
-	*ade=temp;
-	printf("The value of d and e is %d and %d\n",d,e);
-
+  int d = 4;
+  int e = 8;
+  int* ptrtod = &d;
+  int* ptrtoe = &e;
+  printf("The value of d is %d and the address is %d\n", d, ptrtod);
+  printf("The value of e is %d and the address is %d\n", e, ptrtoe);
+  e = *ptrtod;
+  d = 8;
+  printf("After the swap, the value of d is %d\n", d);
+  printf("After the swap, the value of e is %d\n", e);
 }
