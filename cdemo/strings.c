78c4891f9d05dc960dd8c1ec09d0e84276b9fabc
Gwammer <33038959+Gwammer@users.noreply.github.com>
2019-05-29 18:10:19 -0700

    Update strings.c

--- cdemo/strings.c
+++ cdemo/strings.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	char loopABC[26];
-	char* constantABC = "abcdefghijklmnopqrstuvwxyz";
-	for(int i = 97; i <=122; i++){
-		loopABC[i] = i;
-	}
-	printf("Original: %s,   Loop: %s",constantABC, loopABC);
-if(strcmp(loopABC,constantABC) == 0){
-		printf("The two strings match\n");
-	}else{
-		printf("The strings do not match\n");
-	}
+int main(void) {
+  char string1[26];
+  for (int i = 0; i < 26; i++){
+    string1[i] = 97 + i;
+  }
+  char string2 [] = "abcdefghijklmnopqrstuvwxyz";
+  if (strcmp(string1, string2) == 0)
+     printf("Lowercase strings identical\n");
+  else
+     printf("Lowercase strings not identical\n");
+  for (int i = 0; i < 26; i++){
+    string2[i] = string2[i] - 32;
+  }
+  if (strcmp(string1, string2) == 0)
+     printf("Change didn't work\n");
+  else
+     printf("Change worked\n");
+  
+  char string3[52];
+  strcpy(string3, string1);
+  strcat(string3, string2);
+  printf("Lowercase: %s\n", string1);
+  printf("Uppercase: %s\n", string2);
+  printf("Both: %s\n", string3);
 }
