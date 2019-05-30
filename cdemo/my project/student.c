6334ab62b5c72dbf79136649669ec54a70c3804d
Gwammer <33038959+Gwammer@users.noreply.github.com>
2019-05-29 18:12:17 -0700

    Create student.c

---
+++ cdemo/my project/student.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "student.h"
+
+void printStudent(struct Student* s){
+//    for(int i = 0; i <=count; i++){
+    printf("\n"); 
+    printf("Full Name: %s %s\n",s->fname, s->lname);
+    printf("Age: %d\n",s->age);
+    printf("Student ID: %d\n",s->studentID);
+    printf("\n");
+//        }
+}
